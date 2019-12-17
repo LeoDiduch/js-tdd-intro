2ed62839654cf63c7698369bdf07ab2267c57524
LeoDiduch <leo-diduch_student2019@wilder.school>
2019-12-17 14:43:36 +0100

    step-3 update

--- test/ Rectangle.test.js
+++ test/ Rectangle.test.js
@@ -2,19 +2,19 @@ const assert = require("assert");
 const Rectangle = require("../Rectangle");
 
 describe("Rectangle", () => {
-  it("returns true on instance created with two sides of the same length otherwise return false", () => {
+  it("isSquare(): Check returns true on instance created with two sides of the same length otherwise return false", () => {
     let rect1 = new Rectangle(9, 9);
     assert.strictEqual(rect1.isSquare(), true);
     let rect2 = new Rectangle(9, 5);
     assert.strictEqual(rect2.isSquare(), false);
   });
 
-  it("check that it returns the surface of the rectangle (ex: 25 for a rectangle of 5 by 5).", () => {
+  it("getArea(): Check that it returns the surface of the rectangle (ex: 25 for a rectangle of 5 by 5).", () => {
     let rect3 = new Rectangle(5, 5);
     assert.strictEqual(rect3.getArea(), 25);
   });
 
-  it("check that it returns well the surface of the rectangle (ex: 20 for a rectangle of (5 + 5) * 2 ).", () => {
+  it("rectangle.getPerimeter(): Check that it returns well the surface of the rectangle (ex: 20 for a rectangle of (5 + 5) * 2 ).", () => {
     let rect4 = new Rectangle(5, 5);
     assert.strictEqual(rect4.getPerimeter(), 20);
   });
--- test/capitalizeFirstLetters.test.js
+++ test/capitalizeFirstLetters.test.js
@@ -2,19 +2,19 @@ const assert = require("assert");
 const capitalizeFirstLetters = require("../capitalizeFirstLetters");
 
 describe("capitalizeFirstLetters", () => {
-  it("is a function and accepts one argument", () => {
+  it("Is a function and accepts one argument", () => {
     assert.strictEqual(typeof capitalizeFirstLetters, "function");
     assert.strictEqual(capitalizeFirstLetters.length, 1);
   });
 
-  it("transforms i am learning TDD correctly to I Am Learning TDD", () => {
+  it("Transforms i am learning TDD correctly to I Am Learning TDD", () => {
     assert.strictEqual(
       capitalizeFirstLetters("i am learning TDD"),
       "I Am Learning TDD"
     );
   });
 
-  it("works for an empty string", () => {
+  it("Works for an empty string", () => {
     assert.strictEqual(capitalizeFirstLetters(""), "");
   });
 });
